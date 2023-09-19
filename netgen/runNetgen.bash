6339c2f3dc6031c9d79195d62f22217be7576b9a
James Stine <james.stine@okstate.edu>
2023-09-19 17:34:43 -0500

    clean up some dirs

--- netgen/runNetgen.bash
+++ netgen/runNetgen.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-netgen -batch lvs "../ngspice/$1.spice $1" "../xschem/$1.spice $1" $PDK_ROOT/sky130A/libs.tech/netgen/sky130A_setup.tcl $1.out
+netgen -batch lvs "../magic/$1.spice $1" "../xschem/$1.spice $1" $PDK_ROOT/sky130A/libs.tech/netgen/sky130A_setup.tcl $1.out
