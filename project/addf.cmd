06123b07af9cce8c2de741891bbae9fd0b1808f0
James Stine <james.stine@okstate.edu>
2023-10-22 21:32:26 -0500

    Update project elmeents

---
+++ project/addf.cmd
@@ -0,0 +1,28 @@
+| testvectors
+stepsize 20.0
+h vdd
+l gnd
+ana A B CI S CO
+analyzer
+l A B CI
+s
+h A
+s
+l A
+h B
+s
+h A B
+s
+h CI
+l A B
+s
+h A
+s
+l A
+h B
+s
+h A B
+s
+h CI
+s
+
---
+++ project/addf.cmd~
@@ -0,0 +1,11 @@
+| testvectors
+stepsize 10.0
+h vdd
+l gnd
+ana A Y
+analyzer
+l A
+s
+h A
+s
+
