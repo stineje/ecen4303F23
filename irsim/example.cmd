e60db56a0ca46bb17f5e5eed8f46f4ef75c72445
James Stine <james.stine@okstate.edu>
2023-10-23 09:03:34 -0500

    update example IRSIM

---
+++ irsim/example.cmd
@@ -0,0 +1,25 @@
+| testvectors
+stepsize 20.0
+h vdd
+l gnd
+vector In a b c 
+ana -h in out
+analyzer
+setvector In 000
+s
+setvector In 001
+s
+setvector In 010
+s
+setvector In 011
+s
+setvector In 100
+s
+setvector In 101
+s
+setvector In 110
+s
+setvector In 111
+s
+
+
